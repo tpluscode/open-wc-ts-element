34608ef0b581477b44e4b2e6edcfdd80c6700dca
tpluscode <tpluscode@users.noreply.github.com>
2019-06-16 15:48:07 +0200

    chore: set up customizes @open-wc/linting and @open-wc-testing

---
+++ test/ts-element.test.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+import { html, fixture, expect } from '@open-wc/testing'
+
+import '../src/ts-element'
+
+describe('<ts-element>', () => {
+    it('has a default property heading', async () => {
+        const el = await fixture('<ts-element></ts-element>')
+
+        expect(el.heading).to.equal('Hello world!')
+    })
+
+    it('allows property heading to be overwritten', async () => {
+        const el = await fixture(html`
+      <ts-element heading="different heading"></ts-element>
+    `)
+
+        expect(el.heading).to.equal('different heading')
+    })
+})
