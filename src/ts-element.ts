e16f3362f26e7baaa5ba334f9dae557f585a8459
tpluscode <tpluscode@users.noreply.github.com>
2019-06-16 21:24:24 +0200

    test: add running implementation of the sample test

--- src/ts-element.ts
+++ src/ts-element.ts
@@ -1,3 +1,12 @@
-export default class TsElement {
+import { html, LitElement, property } from 'lit-element'
 
+export default class TsElement extends LitElement {
+    @property({ type: String })
+    public heading: string = 'Hello world!'
+
+    public render() {
+        return html`<h1>${this.heading}</h1><div><slot></slot></div>`
+    }
 }
+
+customElements.define('ts-element', TsElement)
